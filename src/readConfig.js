8e1f6b42091c35379716037c83638cf769f18ccf
Edgar Villegas Alvarado <edgarinvillegas@hotmail.com>
2018-11-13 01:19:22 -0400

    refactor: Now config is based on prototype file: configBase.json.

--- src/readConfig.js
+++ src/readConfig.js
@@ -10,37 +10,11 @@ const logger = require('./lib/logger');
  */
 function readConfig() {
     logger.log('Reading configuration...');
-    const conf_defaults = {
-        "credentials": {
-            "coxEmail": "YOUR_NAME@coxautoinc.com",
-            "coxPassword": "",
-            "mojixEmail": "YOUR_NAME@mojix.com",
-            "mojixPassword": ""
-        },
-        "project": "PRJ0000000",
-        "category": "Development",
-        "defaultHours": {
-            "monday": 8,
-            "tuesday": 8,
-            "wednesday": 8,
-            "thursday": 8,
-            "friday": 8
-        },
-        "exceptionalHours": {
-            "2018-11-01": 0,
-            "2017-12-31:2018-01-01": 0,
-        },
-        "emailSettings": {
-            "to": ["cox_report@mojix.com"],
-            "cc": ["LEAD_NAME@mojix.com"],
-            "bcc": [],
-            "subjectTemplate": "{weekendDate} 1234 PEREZ"
-        }
-    };
-
     const conf_file ='./config.json';
     if( ! fs.existsSync(conf_file) ) {
-        fs.writeFileSync( conf_file, JSON.stringify(conf_defaults, null, 2) );
+        // fs.writeFileSync( conf_file, JSON.stringify(conf_defaults, null, 2) );
+        const configBaseContents = fs.readFileSync(`${__dirname}/configBase.json`, 'utf8');
+        fs.writeFileSync( conf_file, configBaseContents );
         logger.log(`Please edit ${conf_file} and rerun the application`);
         process.exit(1);
     }
