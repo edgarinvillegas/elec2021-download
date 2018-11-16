3435912e59c765cfad4488eaeaa5880c99a10499
Edgar Villegas Alvarado <edgarinvillegas@hotmail.com>
2018-11-16 00:30:59 -0400

    feat: Make credentials.json be created only if config.js imports it

--- src/readConfig.js
+++ src/readConfig.js
@@ -11,14 +11,26 @@ const logger = require('./lib/logger');
  */
 function readConfig() {
     logger.log('Reading configuration...');
-    const credentials_file ='./credentials.json';
     // const missingConfFiles = [];
     const old_config_file = './config.json';
     if( fs.existsSync(old_config_file) ) {
         logger.log(`${old_config_file} is deprecated. Please use config.js`);
     }
 
-    if( ! fs.existsSync(credentials_file) ) {
+    const conf_file ='./config.js';
+    const conf_file_fullpath = process.cwd() + '/config.js';
+    if( ! fs.existsSync(conf_file) ) {
+        // fs.writeFileSync( conf_file, JSON.stringify(conf_defaults, null, 2) );
+        const configBaseContents = fs.readFileSync(`${__dirname}/configBase.static.js`, 'utf8');
+        fs.writeFileSync( conf_file, configBaseContents );
+        logger.log(`${conf_file} created in the current directory. Please edit it and rerun the application`);
+        process.exit(1);
+    }
+
+    // We check if config.js imports credentials file. If so, create the file
+    const credentials_file ='./credentials.json';
+    const configContentsStr = fs.readFileSync(conf_file_fullpath, 'utf8');
+    if( configContentsStr.includes(credentials_file) && !fs.existsSync(credentials_file) ) {
         const defaultCredentials =  {
             coxEmail: 'YOUR_NAME@coxautoinc.com',
             coxPassword: '',
@@ -28,16 +40,8 @@ function readConfig() {
         fs.writeFileSync( credentials_file, JSON.stringify(defaultCredentials, null, 2) );
         logger.log(`${credentials_file} created in the current directory. Please edit it and rerun the application`);
     }
-    const conf_file ='./config.js';
-    if( ! fs.existsSync(conf_file) ) {
-        // fs.writeFileSync( conf_file, JSON.stringify(conf_defaults, null, 2) );
-        const configBaseContents = fs.readFileSync(`${__dirname}/configBase.static.js`, 'utf8');
-        fs.writeFileSync( conf_file, configBaseContents );
-        logger.log(`${conf_file} created in the current directory. Please edit it and rerun the application`);
-        process.exit(1);
-    }
     // nconf.file({file: conf_file});
-    const configJsObj = require(process.cwd() + '/config.js')
+    const configJsObj = require(conf_file_fullpath)
 
     nconf.argv()
         // .env()
